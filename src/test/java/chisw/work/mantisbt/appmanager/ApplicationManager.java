e86f030c672ed842a4748cdc375fdd54517c63f4
Tarrest <reva.htp@gmail.com>
2018-01-24 10:08:19 +0200

    Initial commit

---
+++ src/test/java/chisw/work/mantisbt/appmanager/ApplicationManager.java
@@ -0,0 +1,41 @@
+package chisw.work.mantisbt.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private final Properties properties;
+    WebDriver driver;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+
+    }
+
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        if (browser.equals(BrowserType.FIREFOX)) {
+            System.setProperty("webdriver.gecko.driver", "c:\\Tools\\geckodriver.exe"); //папку с драйверами добавил в path
+            driver = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            System.setProperty("webdriver.chrome.driver", "c:\\Tools\\chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+        //driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.get(properties.getProperty("web.baseUrl"));
+    }
+    public void stop() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/chisw/work/mantisbt/test/TestBase.java
@@ -0,0 +1,31 @@
+package chisw.work.mantisbt.test;
+
+
+import chisw.work.mantisbt.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class TestBase {
+
+/*    protected final static ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));*/
+    protected final static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        app.stop();
+    }
+
+}
